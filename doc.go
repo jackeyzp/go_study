8ea00170e5251148992098e8ef8456a764b5405f
hexiwei <xiwei_he@foxmail.com>
2016-01-30 21:11:40 +0800

    update

---
+++ doc.go
@@ -0,0 +1,6 @@
+// go_study project doc.go
+
+/*
+go_study document
+*/
+package main
---
+++ main.go
@@ -0,0 +1,10 @@
+// go_study project main.go
+package main
+
+import (
+	"fmt"
+)
+
+func main() {
+	fmt.Println("Hello World!")
+}
